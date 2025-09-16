911773fd092d2ebc6a9cc7b6709bcd5e6156ed21
오준영 <ohjunyoung@email.com>
2025-09-16 17:58:50 +0900

    파이썬

--- advance/07_static_member.py
+++ advance/07_static_member.py
@@ -11,27 +11,27 @@ class Robot:
     def std_count(cls):
         print(f'클래스 메서드 : {cls.count}')
 
-r1 = Robot()
-r2= Robot()
+r1 = Robot()  #1
+r2= Robot()   #0
 # r1 과 r2 는 서로 다른 객체이므로 count 를 건드렸을때 서로 영향받지 않는다.
 r1.count += 1
-print(f'r1.count : {r1.count}')
-print(f'r2.count : {r2.count}')
+print(f'r1.count : {r1.count}') #1
+print(f'r2.count : {r2.count}') #100
 r2.count = 100
-print(f'r1.count : {r1.count}')
-print(f'r2.count : {r2.count}')
+print(f'r1.count : {r1.count}') #1
+print(f'r2.count : {r2.count}') #100
 
 # 원본의 내용을 고치고 싶다면? 원본으로 직접 가서 고쳐야 한다.
 Robot.count = 1000
 # 원본(static)영역에서 고쳤을때 당연히 복사본(heap)영역에는 영향이 없다.
 
-print(f'r1.count : {r1.count}')
-print(f'r2.count : {r2.count}')
+print(f'r1.count : {r1.count}') #1
+print(f'r2.count : {r2.count}') #100
 r1.how_count()
 r2.how_count()
 
 #마찬가지로 원본의 내용을 확인하고 싶다면 원본영역으로 가서 확인해야 한다.
-print(f'원본 count : {Robot.count}')
+print(f'원본 count : {Robot.count}')  #1000
 
 # 1. 원본영역에서 함수를 실행하니 self 가 없다고 에러가남
 # TypeError: Robot.std_count() missing 1 required positional argument: 'self'
