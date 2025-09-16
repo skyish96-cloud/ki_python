ff9bf312c1b179a26603ebc693c7aa84afb43a91
오준영 <ohjunyoung@email.com>
2025-09-16 14:51:48 +0900

    상속 요약

--- advance/09_super_init.py
+++ advance/09_super_init.py
@@ -30,5 +30,19 @@ t = Teacher('김철수',33,50000000)
 # 5. name 과 age 는 부모것 이지만 내것처럼 내 객체에서 가져다 쓸 수 있게 된다.
 print(f'{t.name}({t.age}) - {t.salary}')
 
+# t라는 변수에 Teacher에 변수를 입력.
+# super().__init__(name, age)
+# 위에 객체를 아래 부모 클래스에 입력해서 부모 객체를 불러옴
+# def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+# 위에 부모에서 받은 것으 렞외하고 salary만 자식의 객체이지만
+# 부모의 객체를 상속받아서 부모의 객체를 자기 객체처럼 사용
+#     def __init__(self, name, age, salary):
+#     self.salary = salary
 
-#ㅋㅋ
+
+# 상속의 특징
+# - 부모의 메서드의 기능이 마음에 들지 않거나 아쉬운 경우가 있다.
+# - 마찬가지로 부모님께 상속받은 차가 마음에 들지 않는 경우가 있다.
+#                 그럴 경우 우리는 자동차 튜닝을 한다 (Override라고 표현한다.)
