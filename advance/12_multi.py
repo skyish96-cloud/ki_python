57cf3387413dbd0c8925c71f4711dc8ed964dcd4
오준영 <ohjunyoung@email.com>
2025-09-16 15:47:51 +0900

    다중 예외 처리 방식

---
+++ advance/12_multi.py
@@ -0,0 +1,17 @@
+# try코드 안에서 여러개의 예외가 발생한 경우...
+
+# 1. 생겨난 예외마다 각기 다른 처리를 해주고 싶을 때
+try:
+    pass
+except IOError:
+    pass
+except KeyboardInterrupt:
+    pass
+except ValueError:
+    pass
+
+# 2. 어떤 예외가 발생하던지 동일한 처리를 하고 싶을 때
+try:
+    pass
+except Exception:
+    pass
