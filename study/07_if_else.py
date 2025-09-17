6b5cea21f9c413c6b34d959370c6cb28d237585f
오준영 <ohjunyoung@email.com>
2025-09-17 09:27:15 +0900

    ifelse문

---
+++ study/07_if_else.py
@@ -0,0 +1,10 @@
+# if ~ else 문
+# if문의 조건이 참이면 if문 종속 코드를 실행
+# 조건이 거짓이면 else 문의 종속 코드를 실행
+
+num = 4
+
+if num%2 == 0:
+    print(f"{num}은 짝수 입니다.")
+else: # else 는 조건을 사용할 수 없다.
+    print(f"{num}은 홀수 입니다.")
